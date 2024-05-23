4551ee09a2dd7fd4d98328c58af399c7d921a76a
최도훈 <chlehgns123@naver.com>
2024-05-23 17:59:35 +0900

    [Config] githubActions 수정 - master에 머지 및 푸시 후에 작동

--- index.js
+++ index.js
@@ -86,9 +86,9 @@ window.addEventListener("load", async () => {
   postListElement.appendChild(loadingMessage);
 
   try {
-    // 주석 해제 Start
+    // 주석 해제 되나 Start
     // const posts = await fetchLatestPosts();
-    // 주석 해제 End
+    // 주석 해제 되나 End
 
     // 로딩 스피너 삭제
     postListElement.removeChild(loadingSpinner);
